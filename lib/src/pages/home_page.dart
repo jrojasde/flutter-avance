d554aa850c6c2a5ddd0f00719f1b83952c9f2993
jrojasde <jos1991fernando@gmail.com>
2021-08-12 17:01:20 -0500

    seccion 5 FH
    done

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,44 +1,33 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 25);
 
-  final estiloTexto = new TextStyle( fontSize: 25 );
-
+  final conteo = 10;
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-
-    appBar: AppBar(
-      title: Text('Titulo'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Column(
-
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
-          Text('Numero de clicks:', style: estiloTexto ),
-          Text('0', style: estiloTexto ),
-        ],
-
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: () {
-
-        print('Hello word');
-
-      },
-    ),
-  );
- 
-}
-
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numero de clicks:', style: estiloTexto),
+            Text('$conteo', style: estiloTexto),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hello word');
+        },
+      ),
+    );
+  }
 }
