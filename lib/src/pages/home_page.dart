d21728da2ab8bab7d1c63f0cc2d9bc35e149e1e5
Jose <jos1991fernando@gmail.com>
2021-08-12 11:44:36 -0500

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,33 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+
+    appBar: AppBar(
+      title: Text('Titulo'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Column(
+
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Text('Numero de clicks:'),
+          Text('0'),
+        ],
+
+      ),
+    ),
+  );
+ 
+}
+
+
+}
